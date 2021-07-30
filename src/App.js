429568ea07c316a16f6babab8377d65d94057c92
James King <james_andrew_king@hotmail.co.uk>
2021-07-30 12:42:29 -0700

    added a basic header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.scss';
-import Router from './router/Router';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/header/Header';
+import LoginPage from './pages/login/Login';
+import HomePage from './pages/home/Home';
+import RegisterPage from './pages/register/Register';
+import {
+  homeURL,
+  loginURL,
+  registerURL
+} from './utilities/routerURLs'
 
 function App() {
   return (
     <div className="App">
-      <Router />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path={homeURL} component={HomePage} />
+          <Route path={loginURL} component={LoginPage} />
+          <Route path={registerURL} component={RegisterPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+import './Header.scss';
+import { NavLink } from 'react-router-dom';
+import {
+    homeURL,
+    loginURL,
+    registerURL
+} from '../../utilities/routerURLs';
+
+function Header() {
+    return (
+        <header className="header">
+            <nav className="header__nav">
+                <ul className="header__nav-items">
+                    <li className="header__nav-item">
+                        <NavLink to={homeURL} activeClassName="header__link--selected">Home</NavLink>
+                    </li>
+                    <li className="header__nav-item">
+                        <NavLink to={loginURL} activeClassName="header__link--selected">Login</NavLink>
+                    </li>
+                    <li className="header__nav-item">
+                        <NavLink to={registerURL} activeClassName="header__link--selected">Register</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
