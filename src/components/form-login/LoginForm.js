7fa8186753c68815e5095ab5e3d3b9fbb38f3584
James King <james_andrew_king@hotmail.co.uk>
2021-07-30 11:22:05 -0700

    built login form skeleton

---
+++ src/components/form-login/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+    const [ username, setUsername ] = useState('');
+    const [ usernameError, setUsernameError ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ emailError, setEmailError ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ passwordError, setPasswordError ] = useState('');
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        console.log('submitted');
+        if (!formValid()) return console.log('frm is not valid');
+    };
+
+    const formValid = () => {
+        if (usernameError || emailError || passwordError) {
+            return false;
+        }
+        return true;
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <p>Username</p>
+                <label htmlFor="username">
+                    <input type="input" name="username" value={username} onChange={e => setUsername(e.target.value)}/>
+                </label>
+                <div>
+                    {usernameError && <p>error message</p>}
+                </div>
+            </div>
+            <div>
+                <p>Email</p>
+                <label htmlFor="email">
+                    <input type="input" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <div>
+                    {emailError && <p>error message</p>}
+                </div>
+            </div>
+            <div>
+                <p>Password</p>
+                <label htmlFor="password">
+                    <input type="input" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+                <div>
+                    {passwordError && <p>error message</p>}
+                </div>
+            </div>
+            <div>
+                <button type="submit">Register</button>
+                <button type="button">Reset Form</button>
+            </div>
+        </form>
+}
+
+export default LoginForm;
--- src/components/form-register/RegisterForm.js
+++ src/components/form-register/RegisterForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import FormInput from '../form-input/FormInput';
 
 function RegisterForm() {
 
