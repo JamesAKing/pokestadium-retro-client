ceecac68fda38e41be78afcb710217164a6afe3e
James King <james_andrew_king@hotmail.co.uk>
2021-08-03 17:08:16 -0700

    minor code clean up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import LoginPage from './pages/login/Login';
 import HomePage from './pages/home/Home';
 import RegisterPage from './pages/register/Register';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { useAuth } from './contexts/AuthContext';
 import {
   homeURL,
   loginURL,
@@ -28,7 +28,7 @@ function App() {
             <Route path={loginURL} component={LoginPage} />
             <Route path={registerURL} component={RegisterPage} />
           </Switch>
-          
+
         </BrowserRouter>
     </div>
   );
