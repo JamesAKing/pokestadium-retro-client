2d9fcda108b89ee66e3626757992334b4a4a1dd5
James King <james_andrew_king@hotmail.co.uk>
2021-08-04 15:07:03 -0700

    logoutUser fn now deletes tokens currently in the database and logouts the user out

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import './Header.scss';
+import { useAuth } from '../../contexts/AuthContext';
 import { NavLink } from 'react-router-dom';
 import {
     homeURL,
@@ -8,6 +9,9 @@ import {
 } from '../../utilities/routerURLs';
 
 function Header() {
+
+    const { user, logoutUser } = useAuth();
+
     return (
         <header className="header">
             <nav className="header__nav">
@@ -15,16 +19,16 @@ function Header() {
                     <li className="header__nav-item">
                         <NavLink to={homeURL} activeClassName="header__link--selected">Home</NavLink>
                     </li>
-                    <li className="header__nav-item">
+                    {!user && <li className="header__nav-item">
                         <NavLink to={loginURL} activeClassName="header__link--selected">Login</NavLink>
-                    </li>
-                    <li className="header__nav-item">
+                    </li>}
+                    {!user && <li className="header__nav-item">
                         <NavLink to={registerURL} activeClassName="header__link--selected">Register</NavLink>
-                    </li>
-                    {/* Temp Link */}
-                    <li className="header__nav-item">
+                    </li>}
+                    {user && <li className="header__nav-item">
                         <NavLink to={dashboardURL} activeClassName="header__link--selected">Dashboard</NavLink>
-                    </li>
+                    </li>}
+                    {user && <li className="header__nav-item"><button type="button" onClick={logoutUser}>Log Out</button></li>}
                 </ul>
             </nav>
         </header>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext, createContext } from 'react';
 import jwt_decode from 'jwt-decode';
 import axios from 'axios';
+import { logoutURL } from '../utilities/apiURLs';
 
 const AuthContext = createContext();
 
@@ -8,8 +9,10 @@ export function useAuth() {
     return useContext(AuthContext);
 };
 
+
 export function AuthProvider({ children }) {
     const [ user, setUser ] = useState();
+    console.log(user);
     // const [ loading, setLoading ] = useState(true);
 
     // Functions determing how client handles auth and state
@@ -23,7 +26,16 @@ export function AuthProvider({ children }) {
         });
     };
 
-    const logoutUser = () => {};
+    const logoutUser = async () => {
+        try {
+            const resp = await axios.delete(logoutURL, { data : {token : user.token} });
+            setUser(null);
+        } catch (err) {
+            console.log(err);
+        };
+        // delete jwt token + refresh token
+        // setUser(null);
+    };
 
     useEffect(() => {
         const unsubscribe = user => {
