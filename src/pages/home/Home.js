030315295c30e59ecd613340ca877667f2aa66c7
James King <james_andrew_king@hotmail.co.uk>
2021-08-09 16:29:56 -0700

    styled header

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,7 @@ function HomePage() {
     return (
         <main className="home">
             HOME PAGE
-            <PokeballLoading />
+            {/* <PokeballLoading /> */}
         </main>
     );
 }
