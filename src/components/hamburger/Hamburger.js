f0795216a20ed0bda6352644981b1523f22c968f
James King <james_andrew_king@hotmail.co.uk>
2021-08-09 18:46:50 -0700

    added and gave temp styling to links on hamburger menu

--- src/components/hamburger/Hamburger.js
+++ src/components/hamburger/Hamburger.js
@@ -1,9 +1,18 @@
 import './Hamburger.scss'
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import {
+    homeURL,
+    loginURL,
+    registerURL,
+    dashboardURL
+} from '../../utilities/routerURLs';
 
 function Hamburger() {
 
-    const [ menuVisible, setMenuVisible ] = useState(false);
+    const { user, logoutUser } = useAuth(); 
+    const [ menuVisible, setMenuVisible ] = useState(true);
 
     console.log(menuVisible)
 
@@ -14,9 +23,25 @@ function Hamburger() {
                 <div className="hamburger__icon-dot"></div>
                 <div className="hamburger__icon-dot"></div>
             </div>
-            <div className={`hamburger__menu ${menuVisible ? "hamburger__menu--active" : "hamburger__menu--inactive"}`}>
-
-            </div>
+            <nav className={`hamburger__nav ${menuVisible ? "hamburger__nav--active" : "hamburger__nav--inactive"}`}>
+                <ul className="hamburger__nav-items">
+                    <li className="hamburger__nav-item">
+                        <NavLink className="hamburger__link" exact to={homeURL} activeClassName="hamburger__link--selected" onClick={() => setMenuVisible(false)}>Home</NavLink>
+                    </li>
+                    {!user && <li className="hamburger__nav-item">
+                        <NavLink className="hamburger__link" to={loginURL} activeClassName="hamburger__link--selected" onClick={() => setMenuVisible(false)}>Login</NavLink>
+                    </li>}
+                    {!user && <li className="hamburger__nav-item">
+                        <NavLink className="hamburger__link" to={registerURL} activeClassName="hamburger__link--selected" onClick={() => setMenuVisible(false)}>Register</NavLink>
+                    </li>}
+                    {user && <li className="hamburger__nav-item">
+                        <NavLink className="hamburger__link" to={dashboardURL} activeClassName="hamburger__link--selected" onClick={() => setMenuVisible(false)}>Dashboard</NavLink>
+                    </li>}
+                    {user && <li className="hamburger__nav-item">
+                        <button type="button" onClick={logoutUser}>Log Out</button>
+                    </li>}
+                </ul>
+            </nav>
         </div>
     );
 }
